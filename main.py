92e1b486aa01dcdc8bee595156f5531fd2317db6
jakeFlak <113362895+JakeFlakne10@users.noreply.github.com>
2022-10-04 09:42:38 -0500

    1

---
+++ main.py
@@ -0,0 +1,20 @@
+from re import A
+
+# my attempt
+def countA (x):
+    aCount = 0
+    if x [""] == a:
+        aCount = + 1
+    for i in range(0,aCount):
+        print(aCount[i])
+
+print(countA("germaaaaaany"))
+
+# working
+def countB(x):
+    count = 0
+    for i in range(0,len(x)):
+        if x[i == "b"]:
+            count = count + 1
+    print(count)
+print(countB("babies"))
